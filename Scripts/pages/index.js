6602b2a3e1e3af9ff5f49262af202be223f68856
Stikeu <Nicolas60200@live.fr>
2022-10-18 19:18:47 +0200

    Avancement SearchByKey

--- Scripts/pages/index.js
+++ Scripts/pages/index.js
@@ -10,38 +10,108 @@ import {
 
 class indexTemplate {
     constructor() {
-        this.recetteApi = new recetteApi()
-        this.listRecette = []
+        this.recetteApi = new recetteApi();
+        this.listRecette = [];
+        this.set = new Set();
+        this.set_appreils = new Set();
+        this.set_ustensiles = new Set();
+        this.userCardDOM = [];
     }
+
     displayRecette() {
         this.listRecette = this.recetteApi.getRecette();
+        
         const recetteSection = document.querySelector(".recette_section");
-        const ingredientsFilter = document.querySelector(".ingredientsFilter");
         this.listRecette.forEach((recette) => {
             const recetteModel = new recetteFactory(recette);
-            const recetteFilter = new ingredientsFactory(recette);
-            const filter = recetteFilter.getIngredientsMenu();
-            const userCardDOM = recetteModel.getRecetteCardDOM();
-            ingredientsFilter.appendChild(filter);
-            recetteSection.appendChild(userCardDOM);
+            this.userCardDOM = recetteModel.getRecetteCardDOM();
+            recetteSection.appendChild(this.userCardDOM);
         });
     }
 
-    searchByKey(key) {
-        let recipesResult = [];
-        for (let i = 0; i < this.listRecette.length; i++) {
-            if (this.listRecette[i].name === key) {
-                recipesResult.push(this.listRecette[i])
-            }
-            for (let j = 0; j < this.listRecette[i].ingredients.length; j++) {
-                if (this.listRecette[i].ingredients[j].ingredient === key) {
-                    recipesResult.push(this.listRecette[i]);
-                }
-            }
-        }
+    displayFilter(recettes) {
+        const ingredientsFilter = document.querySelector(".ingredientsFilter");
+        recettes.forEach(recette => {
+            recette.ingredients.forEach(ingredients => {
+                this.set.add(ingredients.ingredient.toLowerCase());
+            })
+        });
+        this.set.forEach(element => {
+            const recetteFilter = new ingredientsFactory(element);
+            const filter = recetteFilter.getIngredientsMenu();
+            ingredientsFilter.appendChild(filter);
+        })
     }
+
+    displayFilterAppareils(recettes) {
+        const appareilsFilter = document.querySelector(".appareilsFilter");
+        recettes.forEach(recette => {
+            this.set_appreils.add(recette.appliance.toLowerCase());
+        });
+        this.set_appreils.forEach(element => {
+            const recetteFilter = new ingredientsFactory(element);
+            const filter = recetteFilter.getIngredientsMenu();
+            appareilsFilter.appendChild(filter);
+        })
+        
+    }
+
+    displayFilterUstensiles(recettes) {
+        const ustensilesFilter = document.querySelector(".ustensilesFilter");
+        recettes.forEach(recette => {
+            recette.ustensils.forEach(ustensils => {
+                this.set_ustensiles.add(ustensils.toLowerCase());
+            })
+        });
+            this.set_ustensiles.forEach(element => {
+                const recetteFilter = new ingredientsFactory(element);
+                const filter = recetteFilter.getIngredientsMenu();
+                ustensilesFilter.appendChild(filter);
+            })
+            
+    }
+
+    searchByKey(){
+        const searchInput = document.querySelector("[data-search]");
+        searchInput.addEventListener("input", e => {
+           const value = String (e.target.value).toLowerCase();
+            if ( value.length >= 3 ) {
+                // console.log(this.listRecette);
+                var listResult = [];
+                for (let i = 0; i < this.listRecette.length; i++) {
+                    console.log(value)
+                    if (this.listRecette[i].name.toLowerCase().includes(value) || this.listRecette[i].description.toLowerCase().includes(value)) {
+                        listResult.push(this.listRecette[i]);
+                   } 
+                }
+                console.log(listResult);
+            }
+        })
+    }
+    // recherche ingredients et elimination duplication et affichage et avancer fiche
+    // searchByKey(key) {
+    //     let recipesResult = [];
+    //     for (let i = 0; i < this.listRecette.length; i++) {
+    //         if (this.listRecette[i].name === key) {
+    //             recipesResult.push(this.listRecette[i])
+    //         }
+    //         for (let j = 0; j < this.listRecette[i].ingredients.length; j++) {
+    //             if (this.listRecette[i].ingredients[j].ingredient === key) {
+    //                 recipesResult.push(this.listRecette[i]);
+    //             }
+    //         }
+    //     }
+    // }
+    
+    // récuperer recette input match
+
     async init() {
+        var apiRecette = this.recetteApi.getRecette();
         this.displayRecette();
+        this.displayFilter(apiRecette);
+        this.displayFilterAppareils(apiRecette);
+        this.displayFilterUstensiles(apiRecette);
+        this.searchByKey();
     }
 }
 
